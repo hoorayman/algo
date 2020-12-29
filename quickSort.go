87a58e098b1dd4ead89a439a84689cfbd01c9ab1
hoo <813458665@qq.com>
2020-12-29 19:16:42 +0800

    update quick sort

--- quickSort.go
+++ quickSort.go
@@ -6,51 +6,52 @@ import (
 	"time"
 )
 
-func partition(s []int) ([]int, []int) {
-	length := len(s)
-	if length < 2 {
-		return nil, s
-	}
-	r := rand.Intn(length)
-	s[0], s[r] = s[r], s[0]
+func quickSort(a []int) {
+	rand.Seed(time.Now().UnixNano())
+	quick(a)
+}
 
-	i, j := 1, length-1
+func quick(a []int) {
+	if len(a) > 1 {
+		l, r := partition(a)
+		quick(l)
+		quick(r)
+	}
+}
+
+func partition(a []int) ([]int, []int) {
+	r := rand.Intn(len(a))
+	a[0], a[r] = a[r], a[0] // fix the random pivot to a[0], for easy compare
+
+	i, j := 1, len(a)-1
 	for {
-		for ; i < length && s[i] < s[0]; i++ {
+		for ; i < len(a) && a[i] < a[0]; i++ { // ignore head unnecessary value, cannot use a[i] <= pivot because will cause unbalanced partition, for example [5 4 5 4 5] divide to [5 4 5 4 5] and []
 		}
-		for ; i < j && s[j] > s[0]; j-- {
+		for ; j > 0 && a[j] > a[0]; j-- { // ignore tail unnecessary value, cannot use a[j] >= pivot because will cause unbalanced partition
 		}
-		if i < length && i < j {
-			s[i], s[j] = s[j], s[i]
-			i++
+		if i < j {
+			a[i], a[j] = a[j], a[i] // two-way exchange, in case for most values are same
+			i++                     // even if most values are same, we can divide balance, because i++,j-- and i<j, i and j go in opposite direction
 			j--
 		} else {
 			break
 		}
 	}
-	s[0], s[i-1] = s[i-1], s[0]
-	if i < length {
-		return s[:i], s[i:]
+	if i == len(a) { // if this happen, the pivot is the greatest value in left. if use renturn a[:i], a[i:] there a[i:] will be empty [], cause unnecessary or endless partition loop
+		a[0], a[i-1] = a[i-1], a[0] // put pivot to rightmost because it is the greatest value
+		i--
 	}
-	return s[:i-1], s[i-1:]
-}
-
-func quickSort(s []int) {
-	left, right := partition(s)
-	if len(left) > 1 {
-		quickSort(left)
-	}
-	if len(right) > 1 {
-		quickSort(right)
-	}
+	return a[:i], a[i:]
 }
 
 func main() {
-	s := make([]int, 100000)
-	for i := range s {
-		s[i] = rand.Int()
+	a := make([]int, 100000)
+	for i := range a {
+		a[i] = rand.Int()
 	}
+	// fmt.Println(a)
 	t1 := time.Now()
-	quickSort(s)
+	quickSort(a)
 	fmt.Println(time.Now().Sub(t1))
+	// fmt.Println(a)
 }
