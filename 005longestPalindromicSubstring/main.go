5520210659270468792f169d45e04a1ee25242f1
huhao <huhao@ruijie.com.cn>
2021-11-27 15:46:46 +0800

    add longestPalindromicSubstring

---
+++ 005longestPalindromicSubstring/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func longestPalindrome(s string) string {
+	transToBytes := []byte(s)
+	charPosition := map[byte][]int{}
+
+	for i := 0; i < len(transToBytes); i++ {
+		charPosition[transToBytes[i]] = append(charPosition[transToBytes[i]], i)
+	}
+
+	maxSubstrLen := 0
+	beginIndex, endIndex := 0, 0
+	for _, indexs := range charPosition {
+		for span := len(indexs); span > 0; span-- {
+			for i := 0; i+span <= len(indexs); i++ {
+				if indexs[i+span-1]-indexs[i]+1 > maxSubstrLen &&
+					isPalindrome(transToBytes[indexs[i]:indexs[i+span-1]+1]) {
+					maxSubstrLen = indexs[i+span-1] - indexs[i] + 1
+					beginIndex, endIndex = indexs[i], indexs[i+span-1]
+				}
+			}
+		}
+	}
+	return string(transToBytes[beginIndex : endIndex+1])
+}
+
+func isPalindrome(s []byte) bool {
+	for i, j := 0, len(s)-1; i < j; i++ {
+		if s[i] != s[j] {
+			return false
+		}
+		j--
+	}
+
+	return true
+}
+
+func main() {
+	fmt.Println(longestPalindrome("aaabaa"))
+}
